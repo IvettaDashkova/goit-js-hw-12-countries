1f5f4784497cd712f7e484ed3223daeb7d1360d3
IvettaDashkova <ivettazakonno@gmail.com>
2021-01-10 01:13:15 +0200

    исправляет ошибки

--- src/index.js
+++ src/index.js
@@ -1,52 +1,66 @@
 import debounce from '../node_modules/lodash.debounce';
 
-import { defaults, error } from '@pnotify/core';
+import { defaults, info, success, error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/BrightTheme.css';
+defaults.delay = 1000;
+
 import './main.scss';
-defaults.delay = 3000;
 
+const refs = {
+  input: document.querySelector('#searchCountry'),
+  container: document.querySelector('.containerCountry'),
+  country: '',
+};
 
-const input = document.querySelector('#searchCountry');
-const container = document.querySelector('.containerCountry');
+function onlyCountry(data) {
+  const cardCountry = data.map(element => {
+    const textContent = `<h2 data-description="name">${element.name}</h2><div class="content"><div class="textcontent"><p data-description="capital"><span>Capital: </span>${element.capital}</p><p data-description="population"><span>Population: </span>${element.population}</p><ul data-description="languages"><span>Languages: </span></ul></div> <img src="${element.flag}"></div>`;
+    refs.container.insertAdjacentHTML('afterbegin', textContent);
 
-let country = '';
+    const listLang = element.languages.forEach(language => {
+      const item = document.createElement('li');
+      item.textContent = language.name;
+      refs.container
+        .querySelector('[data-description ="languages"]')
+        .append(item);
+    });
+  });
+}
+function listCountries(data) {
+  const list = document.createElement('ul');
+  const items = data.map(element => {
+    const item = document.createElement('li');
+    item.textContent = element.name;
+    list.append(item);
+  });
+  refs.container.append(list);
+}
 
 function searchCountry() {
-  container.innerHTML = '';
-  country = input.value;
-  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+  refs.container.innerHTML = '';
+  refs.country = refs.input.value;
+  fetch(`https://restcountries.eu/rest/v2/name/${refs.country}`)
     .then(response => response.json())
     .then(data => {
       if (data.length === 1) {
-        const oneCountry = data.map(element => {
-          const cardCountry = `<h2 data-description="name">${element.name}</h2><div class="content"><div class="textcontent"><p data-description="capital"><span>Capital: </span>${element.capital}</p><p data-description="population"><span>Population: </span>${element.population}</p><ul data-description="languages"><span>Languages: </span></ul></div> <img src="${element.flag}"></div>`;
-          container.insertAdjacentHTML('afterbegin', cardCountry);
-
-          const addLang = element.languages.forEach(lang => {
-            const point = document.createElement('li');
-            point.textContent = lang.name;
-            container
-              .querySelector('[data-description ="languages"]')
-              .append(point);
-          });
+        onlyCountry(data);
+        success({
+          text: ' Your query is correct!',
         });
       }
       if (data.length > 2 && data.length <= 10) {
-        const list = document.createElement('ul');
-        const listCountries = data.map(element => {
-          const item = document.createElement('li');
-          item.textContent = element.name;
-          list.append(item);
+        listCountries(data);
+        info({
+          text: ' For more detailed information please specify the query!',
         });
-        container.append(list);
       }
       if (data.length > 10) {
-        const myError = error({
+        error({
           text: 'Too many matches found. Please enter a more specific query!',
         });
       }
     });
 }
 
-input.addEventListener('input', debounce(searchCountry, 500));
+refs.input.addEventListener('input', debounce(searchCountry, 500));
